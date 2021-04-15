33c72eca189899d17d51ea020a8225b2348a4ebf
trasto <trasto@gabeeos.trasto.com>
2021-04-15 22:37:23 +0200

    refactor , second function

--- main.zig
+++ main.zig
@@ -2,7 +2,7 @@ const std   = @import("std");
 const debug = std.debug;
 
 
-pub fn coder() void{
+pub fn coder() !void{
 
     debug.print("Introducca la cadena a codear:\n", .{});
     var string_coder: [std.mem.page_size]u8 = undefined;
@@ -14,14 +14,31 @@ pub fn coder() void{
         var code = item + 5;
         debug.print("{c}", .{code});
     }
+    debug.print("\n", .{});
 
 }
 
 
 pub fn main () !void{
     
-    var join: bool = true;
-    while (join) : () {
+    var menu: bool = true;
+    while (menu){
+        //var status = try std.os.execveZ("/usr/bin/clear", undefined, undefined);
+        var response: [std.mem.page_size]u8 = undefined;
+        debug.print("------------------\n", .{});
+        debug.print("Codificardor Cesar\n", .{});
+        debug.print("------------------\n", .{});
+        debug.print("1 Codificar(c)\n2 Terminar(t)\n=> ", .{});
+        var std_out = std.io.getStdOut();
+        var length  = try std_out.read(response[0..]);
+        switch (response[0]) {
+            'c'    => { 
+                try coder();
+                },
+            else => {
+                return ;
+                },
+        }
         
     }
     
