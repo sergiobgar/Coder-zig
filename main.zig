74c39d5e89b9f1a2ba2015e96028a1a7b8d924f3
trasto <trasto@gabeeos.trasto.com>
2021-04-15 21:04:22 +0200

    Refactoring code

--- main.zig
+++ main.zig
@@ -1,21 +1,30 @@
 const std   = @import("std");
 const debug = std.debug;
 
-pub fn main () !void{
-    
-    const fruits = [_][]const u8 {"apple", "orange"};
-    debug.print("Size => {}", .{fruits.len});
-    
+
+pub fn coder() void{
+
     debug.print("Introducca la cadena a codear:\n", .{});
     var string_coder: [std.mem.page_size]u8 = undefined;
     var stdout = std.io.getStdOut();
     var lenght_string = try stdout.read(string_coder[0..]);
-    
-    debug.print("La cadena es:\t{s}", .{string_coder[0..lenght_string]});
     debug.print("Size => {}\n", .{string_coder[0..lenght_string].len});
-    
+
     for (string_coder[0..lenght_string]) |item| {
         var code = item + 5;
         debug.print("{c}", .{code});
     }
+
+}
+
+
+pub fn main () !void{
+    
+    var join: bool = true;
+    while (join) : () {
+        
+    }
+    
+    
+    
 }
